d5fd00cc51a56012e9cc9e263fb2a0e0cf1c9f81
SrMotion666 <116975268+SrMotion666@users.noreply.github.com>
2025-07-20 19:08:55 +0800

    Update PetMutationFinder.lua

--- PetMutationFinder.lua
+++ PetMutationFinder.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/yfNC0pasNC/0", true))()
-https://raw.githubusercontent.com/SrMotion666/PetMutationFinder/refs/heads/main/PetMutationFinder.lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SrMotion666/PetMutationFinder/refs/heads/main/PetMutationFinder.lua"))()
